73ebd50b871f0c7ed184077ca35b3cd9d5b70ea2
Jose Barrios <43865749+jbarriospd@users.noreply.github.com>
2025-07-01 17:19:26 -0500

    Add files via upload

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,13 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 import tailwindcss from '@tailwindcss/vite';
-
-import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://me.projectsjb.website',
-  integrations: [
-    preact(),
-  ],
+
   vite: {
     plugins: [tailwindcss()]
   },
-  experimental: {
-    svg: {
-      mode: 'sprite',
-    },
-  },
+
 });
--- src/utils.ts
+++ src/utils.ts
@@ -12,3 +12,55 @@ export function formatDate(date: Date) {
         day: 'numeric',
     }).format(date)
 }
+
+
+// Define the SiteConfig type above its usage
+type SiteConfig = {
+    author: string;
+    title: string;
+    description: string;
+    lang: string;
+    ogLocale: string;
+    date: {
+        locale: string;
+        options: {
+            day: string;
+            month: string;
+            year: string;
+        };
+    };
+};
+
+export const siteConfig: SiteConfig = {
+    // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+    author: 'SRLEOM',
+    // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+    title: 'astro-theme-resume',
+    // Meta property used as the default description meta property
+    description: 'The official Astro Resume Theme',
+    // HTML lang property, found in src/layouts/Base.astro L:18
+    lang: 'es-CO',
+    // Meta property, found in src/components/BaseHead.astro L:42
+    ogLocale: 'en_GB',
+    // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+    date: {
+        locale: 'es-CO',
+        options: {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric'
+        }
+    }
+}
+
+export const menuLinks: Array<{ title: string; path: string }> = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'Blog',
+        path: '/es/'
+    }
+]
+
