6cfa802eea13733064cbf17c74ad96d20ffbbbd9
Jose Barrios <jose.barrios@pi.edu.co>
2025-07-21 17:32:31 -0500

    fix: add base configuration to astro.config for repository alignment

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jbarriospd.github.io/',
+  base: 'my-repo',
   vite: {
     plugins: [tailwindcss()]
   },
